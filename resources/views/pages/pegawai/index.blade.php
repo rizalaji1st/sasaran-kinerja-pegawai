a4668a2f68fae2b9e78e7191fd9c6630cb892e81
Silvya Amalia <silvyaamalia1@student.uns.ac.id>
2021-06-13 15:16:46 +0700

    menambah dashboard pegawai

--- resources/views/pages/pegawai/index.blade.php
+++ resources/views/pages/pegawai/index.blade.php
@@ -4,28 +4,28 @@
 @section('route-first','Pegawai')
 @section('route-second','Beranda')
 @section('content')
-    <!-- Default box -->
-    <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Hooray</h3>
+<!-- Default box -->
+<div class="card">
+  <div class="card-header">
+    <h3 class="card-title">Hooray</h3>
 
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
-        </div>
-        <div class="card-body">
-          adminLTE udah include gais, selamat lanjut ngoding
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          ini cuma Footer
-        </div>
-        <!-- /.card-footer-->
-      </div>
-      <!-- /.card -->
+    <div class="card-tools">
+      <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+        <i class="fas fa-minus"></i>
+      </button>
+      <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+        <i class="fas fa-times"></i>
+      </button>
+    </div>
+  </div>
+  <div class="card-body">
+    adminLTE udah include gais, selamat lanjut ngoding
+  </div>
+  <!-- /.card-body -->
+  <div class="card-footer">
+    ini cuma Footer
+  </div>
+  <!-- /.card-footer-->
+</div>
+<!-- /.card -->
 @endsection
